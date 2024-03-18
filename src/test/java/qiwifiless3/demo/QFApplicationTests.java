9864fbc06f0db475f0ca0402f939b9f82a0ed505
ashay <ashay@it-spectrum.ru>
2024-03-18 15:42:46 +0300

    разработка

--- src/test/java/qiwifiless3/demo/QFApplicationTests.java
+++ src/test/java/qiwifiless3/demo/QFApplicationTests.java
@@ -3,7 +3,7 @@ package qiwifiless3.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class QFApplicationTests {
 
 	@Test
