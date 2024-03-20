021ffd2e07047efe1f8fe288dc4f89a2b28707dc
ashay <ashay@it-spectrum.ru>
2024-03-20 18:30:31 +0300

    initializing

--- src/test/java/spectrum/qf/QFApplicationTests.java
+++ src/test/java/spectrum/qf/QFApplicationTests.java
@@ -3,7 +3,7 @@ package spectrum.qf;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class QFApplicationTests {
 
 	@Test
