faa93c9aba7e5d16ab47eaf15df4dfc4f68ce074
ashay <ashay@it-spectrum.ru>
2024-03-21 19:14:02 +0300

    разработка

--- Jenkinsfile
+++ Jenkinsfile
@@ -9,6 +9,7 @@ pipeline {
                 script {
                     sh "chmod +x mvnw"
                     sh './mvnw clean package'
+                    sh './mvnw clean verify sonar:sonar -Dsonar.projectKey=s3-file-mover -Dsonar.host.url=http://sonar.it-spectrum.ru -Dsonar.login=sqp_ee79e6043b0f6caa2bd61141ec7e61cdbbb45cd8'
                 }
             }
         }
@@ -22,5 +23,3 @@ pipeline {
                 sh './mvnw deploy'
             }
         }
-    }
-}
--- src/main/java/spectrum/qf/service/QFAwsServiceSMBImpl.java
+++ src/main/java/spectrum/qf/service/QFAwsServiceSMBImpl.java
@@ -1,9 +1,7 @@
 package spectrum.qf.service;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
 import jcifs.smb.SmbException;
 import jcifs.smb.SmbFile;
 import lombok.RequiredArgsConstructor;
@@ -13,10 +11,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 import spectrum.qf.bean.QFFile;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 
 @ConditionalOnProperty(value = "smb.enabled", havingValue = "true")
 @RequiredArgsConstructor
@@ -40,7 +34,6 @@ public class QFAwsServiceSMBImpl implements QFAwsService {
             logger.error("Не удалось вычислить длину файла с именем {}", smbFile.getName());
         }
         String pathFileTo = smbFile.getCanonicalPath();
-        File fileTo = new File(pathFileTo);
         PutObjectRequest putObjectRequest = new PutObjectRequest(awsBucketName, file.getSmbFile().getName(), pathFileTo);
         putObjectRequest.getRequestClientOptions().setReadLimit(1024 * 1024);
         amazonS3.putObject(putObjectRequest);
